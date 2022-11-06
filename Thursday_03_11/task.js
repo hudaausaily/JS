cffc6fc54d99d6450c6a359641c5840f274a80d5
Huda Al-Ausaily <hudaausaily@gmail.com>
2022-11-06 20:50:18 +0200

    Java

---
+++ Thursday_03_11/task.js
@@ -0,0 +1,18 @@
+
+
+let x=prompt("Please enter a value x");
+let y=prompt("Please enter a value y");
+valuex=Number(x)
+valuey=Number(y)
+
+if (valuex>valuey) {
+   console.log ("Hello World");
+   document.write ("Hello World");
+   alert ("Hello World");
+}
+if (valuex<valuey) {
+   console.log ("Goodbye ");
+   document.write ("Goodbye ");
+   alert ("Goodbye ");
+}
+
