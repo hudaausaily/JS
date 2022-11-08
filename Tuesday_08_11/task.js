60d0131b0465cd01a1b02ff4cd28ce0753de4332
Huda Al-Ausaily <hudaausaily@gmail.com>
2022-11-08 23:55:55 +0200

    Java

---
+++ Tuesday_08_11/task.js
@@ -0,0 +1,86 @@
+// 1
+//  [ 1, 7,  9, 45 ]
+//  ["Str" "alex","moh"]
+//  ['the','fox' 'over' lazy, 'dog' ]
+
+// 2
+// var fruits=["Tomato","Banana","Watermelon"]
+// console.log(fruits.indexOf('Tomato'))
+// console.log(fruits.indexOf('Banana'))
+
+// 3
+// let food = ['shawerma', 'pizza', 'burger', 'fried chicken', 'salad'] 
+// let sport = ['yoga', 'football', 'jogging'] 
+// let movie = ['focus', 'inception', 'the godfather', 'shutter island']
+
+// 4
+// function firstOfArray(arr){
+//     return arr[0]
+// }
+// console.log(firstOfArray([1,4,5]))
+// console.log(firstOfArray(["t","u","g","x"]))
+
+// 5
+// function lastOfArray(arr){
+//     return arr[arr.length - 1]
+// }
+// console.log(lastOfArray([1,4,5]))
+// console.log(lastOfArray(["t","u","g","x"]))
+
+// 6
+// var array = [0,5,7,9]
+// console.log()
+// array.shift()
+// array.shift()
+// array.shift()
+// array.unshift(8)
+// array.unshift(6)
+// array.unshift(4)
+// array.unshift(3)
+// array.unshift(1)
+// array.push(10)
+// console.log(array)
+
+// 7
+// var array = [0,5,7,9]
+// console.log()
+// array.shift()
+// array.pop()
+// array.pop()
+// array.push(9)
+// array.push(-7)
+// array.push(3.5)
+
+// console.log(array)
+
+// 8
+// function middleOfArray(arr){
+//     let middle = arr.length / 2
+//     if(arr.length % 2 == 0){
+//         let result = arr.slice(middle -1, middle +1)
+//         return result
+//     }else{
+//         let result = arr.slice(middle , middle + 1)
+//         return result
+//     }
+// }
+// console.log(middleOfArray(["t","u","g","x"]))
+// console.log(middleOfArray([1,4,5]))
+
+
+// 9
+// var animals = ['cat', 'ele', 'bird']
+// animals[0] = 'zebra';
+// animals[1] = 'elephant';
+// animals.pop();
+// console.log(animals)
+
+
+// 10
+// var nums= [1,2,3,8,9]
+// function indexOfArray(nums, pos){
+//     return nums[pos]
+// }
+// console.log(indexOfArray(nums,3))
+// console.log(indexOfArray(nums,1))
+// console.log(indexOfArray(nums,4))
